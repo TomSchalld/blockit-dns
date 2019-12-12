f0280b3574ad852269e62a1ffa9472507bbea240
Tom <tom@riddle.wtf>
2019-12-12 10:04:39 +0100

    adding basic auth

--- backend/src/main/java/com/schalldach/dns/blockit/BlockitApplication.java
+++ backend/src/main/java/com/schalldach/dns/blockit/BlockitApplication.java
@@ -6,11 +6,15 @@
 
 package com.schalldach.dns.blockit;
 
+import com.schalldach.dns.blockit.sec.WebSecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(WebSecurityConfig.class)
 public class BlockitApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(BlockitApplication.class, args);
---
+++ backend/src/main/java/com/schalldach/dns/blockit/sec/WebSecurityConfig.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2019.
+ * Unauthorized copying of this file, via any medium is strictly prohibited Proprietary and confidential
+ * Written by Thomas Schalldach software@schalldach.com
+ */
+
+package com.schalldach.dns.blockit.sec;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+/**
+ * Created by @author Thomas Schalldach on 12/12/2019 software@thomas-schalldach.de.
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withUsername("user")
+                        .password(passwordEncoder().encode("user"))
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
