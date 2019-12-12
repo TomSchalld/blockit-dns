a45a73394bb9f047c0b9709afc59a76485c517e8
Tom <tom@riddle.wtf>
2019-12-12 08:57:48 +0100

    adding base spring boot starter

---
+++ backend/src/main/java/com/schalldach/dns/blockit/BlockitApplication.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2019.
+ * Unauthorized copying of this file, via any medium is strictly prohibited Proprietary and confidential
+ * Written by Thomas Schalldach software@schalldach.com
+ */
+
+package com.schalldach.dns.blockit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlockitApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/schalldach/dns/blockit/BlockitApplicationTests.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2019.
+ * Unauthorized copying of this file, via any medium is strictly prohibited Proprietary and confidential
+ * Written by Thomas Schalldach software@schalldach.com
+ */
+
+package com.schalldach.dns.blockit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
